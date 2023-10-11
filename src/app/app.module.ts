2012e7e05bc47a90865e84e10b11a9ed7fc706c8
Siyam Prasanth <prshiyam@gamil.com>
2023-10-11 12:02:01 +0530

    11-10-2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { ProductListComponent } from './container/product-list/product-list.component';
+import { ProductComponent } from './container/product-list/product/product.component';
+import { SearchComponent } from './container/product-list/search/search.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopHeaderComponent,
+    HeaderComponent,
+    ContainerComponent,
+    ProductListComponent,
+    ProductComponent,
+    SearchComponent,
+    MainMenuComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
